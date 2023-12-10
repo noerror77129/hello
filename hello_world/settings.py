8f412b36b91776e05585ac97663c40fb8e9b3ee4
shenyuguo <771296071@qq.com>
2023-12-10 17:06:35 +0000

    🎈 perf(setting.py): 增加允许访问的ip和ip段

--- hello_world/settings.py
+++ hello_world/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
-
+import ipaddress
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +25,13 @@ SECRET_KEY = "django-insecure-rc@04_mry_3-$@2sq$b9%-9jp6q2eyxf4bsw9&&esj++aw&r)p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["10.67.29.222","127.0.0.1"]
+# 地址段
+network = ipaddress.ip_network("10.67.0.0/16")
+network_hosts = [str(ip) for ip in network.hosts()]
+
+ips = ["10.67.29.222","127.0.0.1"]
+
+ALLOWED_HOSTS = ips + network_hosts
 
 if 'CODESPACE_NAME' in os.environ:
     codespace_name = os.getenv("CODESPACE_NAME")
